e79f25ea6f480437bdaf26d250c9739e484d6ad2
Ângela Almeida <47538505+angelakashya@users.noreply.github.com>
2020-07-02 11:14:49 -0300

    implementando os metodos da classe service

--- server/modules/User/service.ts
+++ server/modules/User/service.ts
@@ -10,15 +10,43 @@ class User implements IUser {
 
     constructor() { }
 
-    create(user: any) { }
+    create(user: any) {
+        return model.User.create(user);
+    }
 
-    getAll(): Bluebird<IUser[]> { }
+    getAll(): Bluebird<IUser[]> {
+        return model.User.findAll({
+            order: ['name']
+        })
+            .then(createUsers);
+    }
 
-    getById(id: number): Bluebird<IUserDetail> { }
+    getById(id: number): Bluebird<IUserDetail> {
+        return model.User.findOne({
+            where: { id }
+        })
+            .then(createUserById);
+    }
 
-    getByEmail(email: string): Bluebird<IUserDetail> { }
+    getByEmail(email: string): Bluebird<IUserDetail> {
+        return model.User.findOne({
+            where: { email }
+        })
+            .then(createUserByEmail);
+    }
 
-    update(id: number, user: any) { }
+    update(id: number, user: any) {
+        return model.User.update(user, {
+            where: { id },
+            fields: ['name', 'email', 'password']
+        });
+    }
 
-    delete(id: number) { }
+    delete(id: number) {
+        return model.User.destroy({
+            where: { id }
+        });
+    }
 }
+
+export default User;
