377e863d266541f3f44f9d761e3db0e9e1b2e504
Ângela Almeida <47538505+angelakashya@users.noreply.github.com>
2020-07-02 13:30:28 -0300

    Criando testes de Unidade para os metodos do service

--- build/tests/unit/unit.test.js
+++ build/tests/unit/unit.test.js
@@ -1,20 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var helpers_1 = require("./config/helpers");
+var service_1 = require("../../server/modules/User/service");
 describe('Testes Unitários do Controller', function () {
     describe('Método Create', function () {
         it('Deve criar um novo Usuário', function () {
+            var novoUsuario = {
+                id: 1,
+                name: 'Novo Usuário',
+                email: 'novousuario@email.com',
+                password: '1234'
+            };
+            var user = new service_1.default();
+            return user.create(novoUsuario)
+                .then(function (data) {
+                helpers_1.expect(data.dataValues).to.have.all.keys(['email', 'id', 'name', 'password', 'updateAt', 'createdAt']);
+            });
         });
     });
     describe('Método Update', function () {
         it('Deve atualizar Usuário', function () {
+            var usuarioAtualizado = {
+                name: 'Nome Atualizado',
+                email: 'atualizado@email.com'
+            };
+            var user = new service_1.default();
+            return user.update(1, usuarioAtualizado).then(function (data) {
+                helpers_1.expect(data[0]).to.be.equal(1);
+            });
         });
     });
     describe('Método GET Users', function () {
         it('Deve retornar uma lista com todos os Usuários', function () {
+            var user = new service_1.default();
+            return user.getAll().then(function (data) {
+                helpers_1.expect(data).to.be.an('array');
+                helpers_1.expect(data[0]).to.have.all.keys(['email', 'id', 'name', 'password']);
+            });
         });
     });
     describe('Método Delete', function () {
         it('Deve deletar um Usuário', function () {
+            var user = new service_1.default();
+            return user.delete(1).then(function (data) {
+                helpers_1.expect(data).to.be.equal(1);
+            });
         });
     });
 });
--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -1,27 +1,56 @@
 import { testDouble, expect } from './config/helpers';
+import User from '../../server/modules/User/service';
 
 describe('Testes Unitários do Controller', () => {
     describe('Método Create', () => {
         it('Deve criar um novo Usuário', () => {
-
+            const novoUsuario = {
+                id: 1,
+                name: 'Novo Usuário',
+                email: 'novousuario@email.com',
+                password: '1234'
+            };
+            const user = new User();
+            return user.create(novoUsuario)
+                .then(data => {
+                    expect(data.dataValues).to.have.all.keys(
+                        ['email', 'id', 'name', 'password', 'updateAt', 'createdAt']
+                    )
+                });
         });
     });
 
     describe('Método Update', () => {
         it('Deve atualizar Usuário', () => {
-
+            const usuarioAtualizado = {
+                name: 'Nome Atualizado',
+                email: 'atualizado@email.com'
+            };
+            const user = new User();
+            return user.update(1, usuarioAtualizado).then(data => {
+                expect(data[0]).to.be.equal(1);
+            })
         });
     });
 
     describe('Método GET Users', () => {
         it('Deve retornar uma lista com todos os Usuários', () => {
-
+            const user = new User();
+            return user.getAll().then(data => {
+                expect(data).to.be.an('array');
+                expect(data[0]).to.have.all.keys(
+                    ['email', 'id', 'name', 'password']
+                )
+            })
         });
     });
 
     describe('Método Delete', () => {
         it('Deve deletar um Usuário', () => {
-
+            const user = new User();
+            return user.delete(1).then(data => {
+                expect(data).to.be.equal(1);
+            })
         });
     });
 });
